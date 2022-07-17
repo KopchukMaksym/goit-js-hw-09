766b7e40a939af6c8115c606d06b0ce5d88f919e
Kopchuk Maksym <kopchuk.maksym@gmail.com>
2022-07-17 14:32:35 +0300

    done 3 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,22 +1,25 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
+
 let intervallId = null;
+stopBtn.disabled = true;
+
 const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
 const addColorRandom = () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
   intervallId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
-    startBtn.disabled = true;
-    if ((startBtn.disabled = true)) {
-      stopBtn.disabled = false;
-    }
   }, 1000);
 };
 
 startBtn.addEventListener('click', addColorRandom);
+
 stopBtn.addEventListener('click', () => {
   clearInterval(intervallId);
   startBtn.disabled = false;
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+const delayInput = document.querySelector('[name="delay"]');
+const stepInput = document.querySelector('[name="step"]');
+const amountInput = document.querySelector('[name="amount"]');
+const btnEl = document.querySelector('button');
+
+let intervallId;
+
+const createPromise = (position, delay) => {
+  const promise = new Promise((resolve, reject) => {
+    intervallId = setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+};
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  const delay = Number(delayInput.value);
+  const step = Number(stepInput.value);
+  const amount = Number(amountInput.value);
+  let timeout = delay;
+  for (let i = 1; i <= amount; i += 1) {
+    const promise = createPromise(i, timeout);
+    promise
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    timeout += step;
   }
-}
+});

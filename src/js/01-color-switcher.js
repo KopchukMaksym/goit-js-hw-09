392ea5a31832d9a50c9517141a2cea86443e35bb
Kopchuk Maksym <kopchuk.maksym@gmail.com>
2022-07-17 12:25:31 +0300

    done 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let intervallId = null;
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+const addColorRandom = () => {
+  intervallId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    if ((startBtn.disabled = true)) {
+      stopBtn.disabled = false;
+    }
+  }, 1000);
+};
+
+startBtn.addEventListener('click', addColorRandom);
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervallId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
